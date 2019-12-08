be6cdeb8c67f2e9baf425904e835b77d617f78c6
water-ink <54940268+water-ink@users.noreply.github.com>
2019-12-08 19:20:59 +0800

    display user name

--- src/main/java/com/lsx/bbs/Controller/AuthorizeController.java
+++ src/main/java/com/lsx/bbs/Controller/AuthorizeController.java
@@ -4,10 +4,12 @@ import com.lsx.bbs.dto.AccessTokenDTO;
 import com.lsx.bbs.dto.GitHubUser;
 import com.lsx.bbs.provider.GitHubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @SuppressWarnings("SpringJavaAutowiredFieldsWarningInspection")
@@ -15,32 +17,42 @@ import java.io.IOException;
 public class AuthorizeController {
 
     @Autowired
-    GitHubProvider gitHubProvider;
+    private GitHubProvider gitHubProvider;
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.uri}")
+    private String clientUri;
+    @Value("${github.client.secret}")
+    private String clientSecret;
 
     @RequestMapping("/callback")
     public String Authorize(@RequestParam(name="code")String code,
-                            @RequestParam(name="state")String state) throws IOException {
+                            @RequestParam(name="state")String state,
+                            HttpServletRequest request) throws IOException {
 
 //        InputStream is = AuthorizeController.class.getClassLoader().getResourceAsStream("authorize.properties");
 //        Properties prop = new Properties();
 //        prop.load(is);
 
-        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setCode(code);
-        accessTokenDTO.setState(state);
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
-        accessTokenDTO.setClient_id("c033421872b2455bf926");
-        accessTokenDTO.setClient_secret("59a539e29f10f80bb66a5e236db015cd17b2c9b2");
-        String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
-        GitHubUser user = gitHubProvider.getUser(accessToken);
-
-        System.out.println(user.getName());
-
 //        accessTokenDTO.setClient_id(prop.getProperty("client_id"));
 //        accessTokenDTO.setRedirect_uri(prop.getProperty("uri"));
 //        accessTokenDTO.setClient_secret(prop.getProperty("client_secret"));
 
-        return "main";
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setRedirect_uri(clientUri);
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
+        GitHubUser user = gitHubProvider.getUser(accessToken);
+        if(user != null){
+            request.getSession().setAttribute("user",user.getName());
+            return "redirect:main";
+        }
+
+
+        return "redirect:main";
     }
 
 }
